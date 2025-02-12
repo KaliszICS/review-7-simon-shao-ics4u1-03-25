d3d3c6635ca92b6f6f858415f9722ca414ae8c59
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-02-12 19:34:15 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,5 +5,80 @@ public class PracticeProblem {
 	}
 
 	//Write your functions here
+	public static String evenOrOdd(int num){
+		String result;
+		if (num %2 == 0){
+			result = "Even";
+		}
+		else{
+			result = "Odd";
+		}
+		return result;
+	}
+	public static String teacherOrStudent(String word){
+		String result;
+		if (word.equals("Kalisz")){
+			result = "Teacher";
+		}
+		else{
+			result = "Student";
+		}
+		return result;
+	}
+	public static int fartherFromZero(int num){
+		int result;
+		if (num == 0){
+		result = num;
+		}
+		else{
+		if (num > 0){
+			result = num + 5;
+		}
+		else{
+			result = num - 5;
+		}
+	}
+	return result;	
+}
+public static String isFive(int num){
+	String result;
+	if (num ==5){
+		result = "The number is Five";
+	}
+	else{
+		result = "The number is not Five";
+}
+return result;	
+}
+public static String positiveOrNegative(double number){
+	String result;
+	if (number >0){
+		result = "Positive";
+	}
+	else{
+		result = "Negative";
+}
+return result;	
+}
+public static String highOrLow(int num){
+	String result;
+	if (num >100){
+		result = "High";
+	}
+	else{
+		result = "Low";
+}
+return result;	
+}
 
+public static String isHello(String word){
+	String result;
+	if (word.equals("Hello")){
+		result = "The word is Hello";
+	}
+	else{
+		result = "The word is not Hello";
+}
+return result;	
+}
 }
